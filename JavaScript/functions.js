9998239a7f2ba936d6d8eebccc0a38b340a002af
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-05-08 12:24:54 -0300

    create-first-section

---
+++ JavaScript/functions.js
@@ -0,0 +1,23 @@
+// Funções para o botão que sobe a página inteira
+window.addEventListener('scroll', () => {
+    const button_up = document.getElementById('up-button')
+    button_up.classList.toggle('upper-button', window.scrollY > 400)
+    changeColorsUpButton(button_up, window.scrollY)
+})
+
+function changeColorsUpButton(element, scroll_value) {
+    const scroll_position = scroll_value + window.innerHeight
+    const document_height = document.documentElement.offsetHeight
+
+    if (scroll_position >= document_height - 150) {
+        element.style.backgroundColor = '#ffffff51'
+        element.style.color = '#000'
+    } else {
+        element.style.backgroundColor = '#00000051'
+        element.style.color = '#fff'
+    }
+}
+
+function resetScroll() {
+    window.scrollTo(0, 0)
+}
