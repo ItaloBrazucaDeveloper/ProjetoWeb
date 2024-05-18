b504cbab7596d524c76bca0f8f6541084b52a9ad
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-05-17 22:57:28 -0300

    update-JavaScript-and-styles

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,55 +1,35 @@
 // Funções para o botão que sobe a página inteira
 window.addEventListener('scroll', () => {
-    const button_up = document.getElementById('up-button')
-    button_up.classList.toggle('upper-button', window.scrollY > 400)
-})
+    const button_up = document.getElementById('up-button');
+    button_up.classList.toggle('upper-button', window.scrollY > 400);
+});
 function resetScroll() {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
 }
-/* function changeColorsUpButton(element, scroll_value) {
-    const scroll_position = scroll_value + window.innerHeight
-    const document_height = document.documentElement.offsetHeight
-
-    if (scroll_position >= document_height - 150) {
-        element.style.backgroundColor = '#ffffff'
-        element.style.color = '#000'
-    } else {
-        element.style.backgroundColor = '#000000'
-        element.style.color = '#fff'
-    }
-} */
-
 // Funções para o carrosel
-function changeChekedRadio(direction) {
+function changeCheckedRadioArrow(direction) {
+    let images = document.querySelectorAll('#slider img');
     for (let i = 0; i <= 4; i++) {
-        if ( document.querySelector('#choicer-service #slide-' + i).checked ) {
+        if ( document.querySelector('#choicer-service #slide-'+ i).checked ) {
+            images[i].classList.remove('selected');
             switch (direction) {
                 case 'right':
                     i == 4 ? i = -1 : i = i;
-                    document.querySelector('#choicer-service #slide-'+ (i + 1) ).checked = true
-                    changeImageService(i + 1)
+                    document.querySelector('#choicer-service #slide-'+ (i + 1) ).checked = true;
+                    images[i + 1].classList.add('selected');
                     break;
                 case 'left':
                     i == 0 ? i = 5 : i = i;
-                    document.querySelector('#choicer-service #slide-'+ (i - 1) ).checked = true
-                    changeImageService(i - 1)
-                    break;
-            }
-            break;
-        }
+                    document.querySelector('#choicer-service #slide-'+ (i - 1) ).checked = true;
+                    images[i - 1].classList.add('selected');
+            } break;
+        } 
     }
 }
-function changeImageService(index) {
-    const background_img = document.getElementById('third-section') 
-    switch (index) {
-        case 0: background_img.style.backgroundImage = 'url("../img/backgrounds/HomemFlexão.jpg")'
-            break;
-        case 1: background_img.style.backgroundImage = 'url("../img/backgrounds/SegurandoBarra.jpg")'
-            break;
-        case 2: background_img.style.backgroundImage = 'url("../img/backgrounds/MulherAbdominal.jpg")'
-            break;
-        case 3: background_img.style.backgroundImage = 'url("../img/backgrounds/PessoasPrancha.jpg")'
-            break;
-        case 4: background_img.style.backgroundImage = 'url("../img/backgrounds/AeroHit.jpg")'
+function changeCheckedRadio(index) {
+    let images = document.querySelectorAll('#slider img');
+    images[index].classList.add('selected');
+    for (let i = 0; i <= 4; i++) {
+        i == index ? i = i : images[i].classList.remove('selected');
     }
 }
