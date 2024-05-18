fb432ca9392356ae2967733a00f7c40eebfc8d0b
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-05-18 16:11:49 -0300

    update-javascript

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -17,11 +17,13 @@ function changeCheckedRadioArrow(direction) {
                     i == 4 ? i = -1 : i = i;
                     document.querySelector('#choicer-service #slide-'+ (i + 1) ).checked = true;
                     images[i + 1].classList.add('selected');
+                    changeServiceInfo(i + 1);
                     break;
                 case 'left':
                     i == 0 ? i = 5 : i = i;
                     document.querySelector('#choicer-service #slide-'+ (i - 1) ).checked = true;
                     images[i - 1].classList.add('selected');
+                    changeServiceInfo(i - 1);
             } break;
         } 
     }
@@ -31,5 +33,29 @@ function changeCheckedRadio(index) {
     images[index].classList.add('selected');
     for (let i = 0; i <= 4; i++) {
         i == index ? i = i : images[i].classList.remove('selected');
+    }
+    changeServiceInfo(index)
+}
+
+function changeServiceInfo(index) {
+
+    fetch('JavaScript/services.txt')
+    .then(res=> {res.text()})
+    .then(res=> {
+        console.log(res)
+    })
+
+    let h1 = document.querySelector('#service-info h1');
+    switch(index) {
+        case 0:
+            h1.innerHTML = 'CrossFit'; break;
+        case 1:
+            h1.innerHTML = 'Cardio'; break;
+        case 2:
+            h1.innerHTML = 'AeroHit'; break;
+        case 3:
+            h1.innerHTML = 'Musculação'; break;
+        case 4:
+            h1.innerHTML = 'Aeróbica';   
     }
 }
