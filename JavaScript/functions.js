d600cf8f896fa0ac4964c82880e164e2c556eef0
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-05-22 20:12:13 +0000

    update-verifyCPF

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,21 +1,38 @@
 // Função para verificar se o CPF é válido
-function isCpfValid(cpf) {
-    let sum = 0, j = 10, digit_verify;
+function isCpfValid() {
+    const cpf_html_element = document.querySelector('#cpf-client');
+    const cpf = document.getElementById('cpf-client').value;
+    let sum = 0, j = 10, limit = 9, verify_count = 0, count = 0, digit_verify, mod;
 
-    if (cpf.lenght() != 11)
-        return false
-    
-    for (let i = 0; i <= 8; i++) {
-        sum += Number(cpf.charAt(i)) * j;
-        j--;
-    }
-    if (sum % 11 == 1 || sum % 11 == 0) {
-        digit_verify = 0;
-    } else {
+    if (cpf == '00000000000' || cpf.length < 11) {
+        cpf_html_element.style.outline = '1px solid #ff0000';
+        return false;
+    } 
+
+    while (count < 2) {
+        for (let i = 0; i <= limit; i++) {
+            sum += Number(cpf.charAt(i)) * j;
+            j--;
+        }
         mod = sum % 11;
-        digit_verify = 11 - mod;
+        if (mod == 0 || mod == 1) {
+            digit_verify = 0;
+        }
+        else {
+            digit_verify = 11 - mod;
+        }
+        if (limit == 8 && digit_verify == cpf.charAt(10)) {
+            verify_count++;
+        }
+        else if (digit_verify == cpf.charAt(11)) {
+            verify_count++;
+        }
+        limit = 10;
+        j = 11;
+        count++;
     }
-    return digit_verify == cpf.charAt(9) 
+    if (verify_count == 2)
+        cpf_html_element.style.outline = '1px solid #00ff00';
+    else
+        cpf_html_element.style.outline = '1px solid #ff0000';
 }
-const cpf_html_element = document.getElementById('cpf-client');
-console.log(isCpfValid(cpf_html_element.value));
