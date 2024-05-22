43d10a1f27724357cc677cf6bfff50ea0b86025b
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-05-22 12:06:57 -0300

    start-cpf-function

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,1 +1,21 @@
-const plans = document.querySelectorAll('#plan-choice input');
+// Função para verificar se o CPF é válido
+function isCpfValid(cpf) {
+    let sum = 0, j = 10, digit_verify;
+
+    if (cpf.lenght() != 11)
+        return false
+    
+    for (let i = 0; i <= 8; i++) {
+        sum += Number(cpf.charAt(i)) * j;
+        j--;
+    }
+    if (sum % 11 == 1 || sum % 11 == 0) {
+        digit_verify = 0;
+    } else {
+        mod = sum % 11;
+        digit_verify = 11 - mod;
+    }
+    return digit_verify == cpf.charAt(9) 
+}
+const cpf_html_element = document.getElementById('cpf-client');
+console.log(isCpfValid(cpf_html_element.value));
