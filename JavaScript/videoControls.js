476ebfd2aa90849e7a7d9ff649813312719ad3f9
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-06-05 09:28:20 -0300

    update-slideShow-and-create-videoControls

---
+++ JavaScript/videoControls.js
@@ -0,0 +1,25 @@
+const screen = document.getElementById("video-promo");
+const video = document.querySelector('#video-promo video');
+const sound_icon = document.querySelector('#video-infos h1 i');
+
+screen.addEventListener('mouseenter', _ => {
+    video.play();
+});
+screen.addEventListener('click', e => {
+    let html_element = e.target;
+    let class_name = html_element.classList.value;
+    if (class_name == 'fa-solid fa-volume-xmark' || class_name == 'fa-solid fa-volume-high') {
+        changeAudio();
+    } else {
+        video.paused ? video.play() : video.pause();
+    }
+});
+function changeAudio() {
+    if (video.muted) {
+        sound_icon.classList.replace('fa-volume-xmark', 'fa-volume-high');
+        video.muted = false;
+    } else {
+        sound_icon.classList.replace('fa-volume-high', 'fa-volume-xmark');
+        video.muted = true;
+    }
+}
