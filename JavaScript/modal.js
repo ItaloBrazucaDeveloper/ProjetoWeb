e0a32e84c5381ac1dcd38d34d334e3f90fa7d3f9
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-06-12 10:19:31 -0300

    add-function-modal-showModal

--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -2,6 +2,6 @@ const button_close = document.querySelector('#close-pop_up');
 const modal = document.querySelector('dialog');
 const body = document.querySelector('body');
 
-body.onload = () => {modal.showModal()};
+body.onload = () => modal.showModal();
 
 button_close.onclick = () => modal.close();
