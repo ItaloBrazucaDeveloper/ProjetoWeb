322beba45b023b0a006534791243a34f795d1644
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-06-15 18:31:16 -0300

    create-sideBar

---
+++ JavaScript/openSideBar.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector('.nav-aside');
+const bars_button = document.getElementById('nav-bars');
+
+bars_button.onclick = () => {
+    sidebar.classList.toggle('active');
+}
