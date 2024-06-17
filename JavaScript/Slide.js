ad190747c080ddca9fbc5d426d86e1807a337fd5
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-06-17 12:17:58 -0300

    fixed-somethings

--- JavaScript/Slide.js
+++ JavaScript/Slide.js
@@ -1,26 +1,19 @@
 const images = document.querySelectorAll('#slider img');
 const inputs_radio = document.querySelectorAll('#choicer-service input[type="radio"]');
 let service_title = document.querySelector('#service-title');
+const services = ['CrossFit', 'Cardio', 'AeroHit', 'Musculação', 'Aeróbica']
 let position = 0;
 
 function changeImageSlideShow(index) {
     position = index;
     images[index].classList.add('selected');
+    service_title.innerText = services[index];
+
     for (let i = 0; i < images.length; i++) {
         if (index != i) {
             images[i].classList.remove('selected');
         }
     }
-    changeServiceInfo(index);
-}
-
-function changeServiceInfo(index) {
-    fetch('JavaScript/services.json')
-        .then(res => res.json())
-        .then(json => {
-            service_title.innerText = json[index]['h1']
-        }
-    );
 }
 
 function automaticSliderShow() {
@@ -28,11 +21,11 @@ function automaticSliderShow() {
     inputs_radio[position].checked = false;
     position++;
     
-    if (position > images.length - 1) {
+    if (position > images.length - 1)
         position = 0;
-    }
+
     images[position].classList.add('selected');
     inputs_radio[position].checked = true;
-    changeServiceInfo(position);
+    service_title.innerText = services[position];
 }
 setInterval(automaticSliderShow, 7000);
--- JavaScript/videoControls.js
+++ JavaScript/videoControls.js
@@ -6,22 +6,4 @@ screen.addEventListener('mouseenter', () => {
     video.play();
 });
 
-screen.onclick = (e) => {
-    let html_element = e.target;
-    let class_name = html_element.classList.value;
-    if (class_name == 'fa-solid fa-volume-xmark' || class_name == 'fa-solid fa-volume-high') {
-        changeAudio();
-    } else {
-        video.paused ? video.play() : video.pause();
-    }
-}
-
-function changeAudio() {
-    if (video.muted) {
-        sound_icon.classList.replace('fa-volume-xmark', 'fa-volume-high');
-        video.muted = false;
-    } else {
-        sound_icon.classList.replace('fa-volume-high', 'fa-volume-xmark');
-        video.muted = true;
-    }
-}
+screen.onclick = () => video.paused ? video.play() : video.pause();
