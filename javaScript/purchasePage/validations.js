96fbd2d370d58507a3de432b74c4374bc2047074
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-06-23 20:40:23 -0300

    update-validation

--- javaScript/purchasePage/validations.js
+++ javaScript/purchasePage/validations.js
@@ -1,161 +1,111 @@
-const regex_num = /[0-9]/g,
-  regex_char = /[a-z|A-Z]/g;
-const form = document.querySelector("#card_purchase-form"),
-  input_name = document.querySelector("#nome_char"),
-  input_num_card = document.querySelector("#num_cartao"),
-  input_date_card = document.querySelector("#dt_venc"),
-  input_verif_num_card = document.querySelector("#CVV"),
-  error_message = document.querySelectorAll(".error_message"),
-  cpf = document.querySelector("#cpf-client"),
-  tel_num = document.querySelector("#client-phone-number"),
-  choice_plan_button = document.querySelector("choice_plan_button");
-let done_inputs_card = 0,
-  done_inputs_id = 0;
+const inputs = document.querySelectorAll(".input-box input"),
+  forms = document.querySelectorAll("form"),
+  confirm_id = document.querySelector("#choice_plan_button"),
+  confirm_card = document.querySelector("#confirm_info-card");
 
-function isOnlyDigit(value) {
-  return value.match(regex_char) ? false : true;
-}
-function isOnlyChar(value) {
-  return value.match(regex_num) ? false : true;
-}
-function verifyInputs(count_inputs) {
-  if (count_inputs == 3) {
-    confirm_info_card.disabled = false;
-  }
-}
+function isCpfValid(cpf) {
+  cpf = cpf.replace(/[.-]/g, "");
+  let count = 1,
+    verify_count = 0,
+    multiplier = 10,
+    limit = 9,
+    digit_verify,
+    sum,
+    mod;
 
-cpf.onfocus = () => {
-  let outline_color = cpf.style.outlineColor;
-  if (outline_color != "lime" || outline_color != "red") {
-    cpf.style.outline = "2px solid tomato";
+  while (count <= 2) {
+    sum = 0;
+    for (let i = 0; i < limit; i++) {
+      sum += Number(cpf.charAt(i)) * multiplier;
+      multiplier--;
+    }
+    mod = sum % 11;
+    digit_verify = mod < 2 ? 0 : 11 - mod;
+    if (count == 1 && digit_verify == cpf.charAt(9)) {
+      verify_count++;
+    } else if (digit_verify == cpf.charAt(10)) {
+      verify_count++;
+    }
+    limit = 10;
+    multiplier = 11;
+    count++;
   }
-}
-cpf.onblur = () => {
-  if (cpf.value == "") {
-    cpf.style.outline = "none";
-  } else if (cpf.style.outlineColor == "lime") {
-    done_inputs_card++;
-  }
-}
-cpf.oninput = () => {
-  let nums = cpf.value;
-  if (!isOnlyDigit(nums)) {
-    cpf.style.outline = "3px solid red";
-    error_message[0].classList.add("active");
-  } else if (nums == "") {
-    error_message[0].classList.remove("active");
-    cpf.style.outline = "2px solid tomato";
-    verifyInputs(3);
-  } else {
-    cpf.style.outline = "2px solid lime";
-    verifyInputs(done_inputs_id);
-  }
+  return verify_count == 2;
 }
 
-tel_num.onfocus = () => {
-  let outline_color = tel_num.style.boxS;
-  if (outline_color != "lime" || outline_color != "red") {
-    tel_num.style.outline = "2px solid tomato";
-  }
+function verifPattern(value, regex) {
+  return value.match(regex) ? true : false;
 }
-tel_num.onblur = () => {
-  if (tel_num.value == "") {
-    tel_num.style.outline = "none";
-  } else if (tel_num.style.outlineColor == "lime") {
-    done_inputs_card++;
+
+function verifInput(input) {
+  if (input.getAttribute("id") == "cpf-client") {
+    if (!isCpfValid(input.value)) return false;
   }
+  return verifPattern(input.value, input.getAttribute("pattern"));
 }
-tel_num.oninput = () => {
-  let nums = tel_num.value;
-  if (!isOnlyDigit(nums)) {
-    tel_num.style.outline = "3px solid red";
-    error_message[0].classList.add("active");
-  } else if (nums == "") {
-    error_message[0].classList.remove("active");
-    tel_num.style.outline = "2px solid tomato";
-  } else {
-    tel_num.style.outline = "2px solid lime";
-    verifyInputs(done_inputs_id);
+
+function applyMaskIn(input) {
+  switch (input.getAttribute("id")) {
+    case "cpf-client":
+      let cpf = input.value;
+      if (cpf.length == 3 || cpf.length == 7) {
+        input.value += ".";
+      } else if (cpf.length == 11) {
+        input.value += "-";
+      }
+      break;
+    case "client-phone-number":
+      let tel = input.value;
+      if (tel.length == 1) {
+        input.value = "(" + input.value;
+      } else if (tel.length == 3) {
+        input.value += ") ";
+      } else if (tel.length == 6 && tel.charAt(5) == 9) {
+        input.value += ".";
+      } else if (tel.length == 11 && tel.charAt(5) == 9) {
+        input.value += "-";
+      } else if (tel.length == 9 && tel.charAt(5) != 9) {
+        input.value += "-";
+      }
+      break;
   }
 }
 
-input_name.onfocus = () => {
-  let outline_color = input_name.style.outlineColor;
-  if (outline_color != "lime" || outline_color != "red") {
-    input_name.style.outline = "2px solid tomato";
-  }
-}
-input_name.onblur = () => {
-  if (input_name.value == "") {
-    input_name.style.outline = "none";
-  } else if (input_name.style.outlineColor == "lime") {
-    done_inputs_card++;
-  }
-}
-input_name.oninput = () => {
-  let chars = input_name.value;
-  if (!isOnlyChar(chars)) {
-    input_name.style.outline = "3px solid red";
-    error_message[0].classList.add("active");
-  } else if (chars == "") {
-    error_message[0].classList.remove("active");
-    input_name.style.outline = "2px solid tomato";
-  } else if (chars.length > 6) {
-    input_name.style.outline = "2px solid lime";
-    verifyInputs(done_inputs_card);
+for (let form of forms) {
+  form.onsubmit = (e) => {
+    e.preventDefault();
   }
 }
 
-input_num_card.onfocus = () => {
-  let outline_color = input_num_card.style.outlineColor;
-  if (outline_color != "lime" || outline_color != "red") {
-    input_num_card.style.outline = "2px solid tomato";
+for (let input of inputs) {
+  input.onfocus = () => {
+    let outline_color = input.style.outlineColor;
+    if (outline_color != "lime" || outline_color != "red") {
+      input.style.outline = "2px solid tomato";
+    }
+  };
+  input.oninput = (e) => {
+    let value = input.value;
+    if (value.length >= input.getAttribute("minlength")) {
+      const id_input = input.getAttribute("id");
+      const message_error = document.querySelector(
+        ".input-box #" + id_input + " ~ .error_message"
+      );
+      if (verifInput(input) === true) {
+        input.style.outline = "2px solid lime";
+        message_error.classList.remove("active");
+      } else {
+        input.style.outline = "3px solid red";
+        message_error.classList.add("active");
+      }
+    }
+    if (e.inputType == "insertText") {
+      applyMaskIn(input);
+    }
   }
-}
-input_num_card.onblur = () => {
-  if (input_num_card.value == "") {
-    input_num_card.style.outline = "none";
-  } else if (input_num_card.style.outlineColor == "lime") {
-    done_inputs_card++;
-  }
-}
-input_num_card.oninput = () => {
-  let nums = input_num_card.value;
-  if (!isOnlyDigit(nums)) {
-    input_num_card.style.outline = "3px solid red";
-    error_message[1].classList.add("active");
-  } else if (nums == "") {
-    error_message[1].classList.remove("active");
-    input_num_card.style.outline = "2px solid tomato";
-  } else {
-    input_num_card.style.outline = "2px solid lime";
-    verifyInputs(done_inputs_card);
-  }
-}
-
-input_verif_num_card.onfocus = () => {
-  let outline_color = input_verif_num_card.style.outlineColor;
-  if (outline_color != "lime" || outline_color != "red") {
-    input_verif_num_card.style.outline = "2px solid tomato";
-  }
-}
-input_verif_num_card.onblur = () => {
-  if (input_verif_num_card.value == "") {
-    input_verif_num_card.style.outline = "none";
-  } else if (input_verif_num_card.style.outlineColor == "lime") {
-    done_inputs_card++;
-  }
-}
-input_verif_num_card.oninput = () => {
-  let code_verif = input_verif_num_card.value;
-  if (!isOnlyDigit(code_verif)) {
-    error_message[2].classList.add("active");
-    input_verif_num_card.style.outline = "3px solid red";
-  } else if (code_verif == "") {
-    error_message[2].classList.remove("active");
-    input_verif_num_card.style.outline = "2px solid tomato";
-  } else {
-    input_verif_num_card.style.outline = "2px solid lime";
-    verifyInputs(done_inputs_card);
+  input.onblur = () => {
+    if (input.value == "") {
+      input.style.outline = "none";
+    }
   }
 }
